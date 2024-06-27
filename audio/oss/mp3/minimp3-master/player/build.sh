b4c8f4bb554e0e4ccffe08ce7919d13fa43c2ce8
zxf <zxf@zxf-ubuntu-22.04.3>
2024-06-27 18:20:15 +0800

    修改build player的脚本命令

--- audio/oss/mp3/minimp3-master/player/build.sh
+++ audio/oss/mp3/minimp3-master/player/build.sh
@@ -1,4 +1,4 @@
-./build-sdl.sh $1
+# ./build-sdl.sh $1
 
-gcc -O2 -o player *.cpp *.c -lstdc++ -lpthread -lm -ldl -LSDL/build-$1 -ISDL/include -lSDL2
-# gcc -O2 -o player *.cpp *.c -lstdc++ -lpthread -lm -ldl -lSDL2
+# gcc -O2 -o player *.cpp *.c -lstdc++ -lpthread -lm -ldl -LSDL/build-$1 -ISDL/include -lSDL2
+gcc -O2 -o player *.cpp *.c -lstdc++ -lpthread -lm -ldl -lSDL2
